6a4c17a94c8b44416c4675bc01a285d8709bb975
programmer <myalkin.vlad@yandex.ru>
2023-07-01 15:23:02 +0300

    - Исправление ошибки вывода количества предметов(теперь все предметы идут по порядку по популярности в стим маркете); - Добавление ограничения вывода данных графика не позднее 30 дней.

--- Steam/steam_market.py
+++ Steam/steam_market.py
@@ -8,11 +8,12 @@ import re
 class SteamMarket:
     def __init__(self):
         self.START_ITEM_INDEX = 0
+        self.COUNT = 20
         self.CURRENCY_RUB = None
         self.html_site = None
         self.get_currency_rub()
         self.event()
-        # self.get_data_item()
+        # self.get_top_list()
 
     def get_currency_rub(self):
         """функция находит курс рубля для доллара в стим"""
@@ -23,10 +24,10 @@ class SteamMarket:
     def get_site(self):
         try:
             steam_market = requests.get(
-                f'https://steamcommunity.com/market/search/render/?query=&start={self.START_ITEM_INDEX}&count=100&search_descriptions=0&sort_column=popular&sort_dir=desc')
+                f'https://steamcommunity.com/market/search/render/?query=&start={self.START_ITEM_INDEX}&count={self.COUNT}&search_descriptions=0&sort_column=popular&sort_dir=desc&appid=730')
             steam_market = steam_market.json()
             self.html_site = bs4.BeautifulSoup(steam_market['results_html'], 'html.parser')
-            self.START_ITEM_INDEX += 100
+            self.START_ITEM_INDEX += 20
         except (TypeError, AttributeError):
             print(f'Стим не хочет пускать, выполняю повторный запрос.')
             time.sleep(60)
@@ -35,20 +36,17 @@ class SteamMarket:
     def get_items(self):
         """функция получает 100 предметов и возвращает их имя, стоимость, ссылку стим"""
         items = []
-        names = self.html_site.find_all(
-            'div', class_="market_listing_row market_recent_listing_row market_listing_searchresult")
+        # names = self.html_site.find_all(
+        #     'div', class_="market_listing_row market_recent_listing_row market_listing_searchresult")
+        #
+        # prices = self.html_site.find_all('span', class_='normal_price')
 
-        prices = self.html_site.find_all('span', class_='normal_price')
-        for values in zip(names, prices):
-            name = values[0].contents[5].contents[1].string
+        for index in range(self.COUNT):
+            item_container = self.html_site.find("div", id=f"result_{index}")
+            name = item_container.find('span', id=f"result_{index}_name").string
+            price = item_container.find('div', class_="market_listing_right_cell market_listing_their_price").contents[1].contents[3].string
             url = f'https://steamcommunity.com/market/listings/730/{str(name.replace(" ", "%20").replace("|", "%7C"))}'
-            try:
-                price = values[1].contents[3].string
-            except IndexError:
-                """При получении тега с ценой может попасться пустой тег.
-                При появлении этого тега возникает ошибка, при которой цикл продолжает работу"""
-                continue
-            item = {'name': name, "price": round(float(price[1:-4])*self.CURRENCY_RUB,2), "url": url}
+            item = {'name': name, "price": round(float(price[1:-4]) * self.CURRENCY_RUB, 2), "url": url}
             print(item)
             self.get_data_item(item)
             items.append(item)
@@ -58,21 +56,13 @@ class SteamMarket:
         items = []
         while True:
             self.get_site()
-            item = self.get_items()
-            items += item
+            self.get_items()
+            # item = self.get_items()
+            # items += item
             time.sleep(2)
 
     def get_data_item(self, item):
         """функция получает данные с сайта маркета стим (НЕ ДОРАБОТАН)"""
-        # https://steamcommunity.com/market/listings/730/Revolution%20Case
-        # item = {'name': 'Sticker | FURIA (Holo) | Paris 2023', 'price': 142.98,
-        #         'url': 'https://steamcommunity.com/market/listings/730/Sticker%20%7C%20FURIA%20(Holo)%20%7C%20Paris%202023'}
-        # sess = requests.Session()
-        # url = sess.post(item['url'])
-        # item_site = bs4.BeautifulSoup(url.text, 'html.parser')
-        # item_site = item_site.find('body')
-        # scripts = item_site.find_all('script')
-
         site = requests.get(item['url'])
 
         steam_graph = re.search(r'var line1=(.+);', site.text)
@@ -80,12 +70,18 @@ class SteamMarket:
 
         values_graph = json.loads(steam_graph)
         prices = []
+        date = datetime.datetime.strptime(values_graph[-1][0][:-4], "%b %d %Y %H") - datetime.timedelta(days=30)
         for values in values_graph:
-            # print(round(float(value[1])*self.CURRENCY_RUB, 2))
-            values[0] = datetime.datetime.strptime(values[0][:-7], "%b %d %Y")
+            values[0] = datetime.datetime.strptime(values[0][:-4], "%b %d %Y %H")
             values[1] = round(float(values[1]) * self.CURRENCY_RUB, 2)
-            if values[0] >= datetime.datetime.strptime('2023-06-01', "%Y-%m-%d"):
+            if values[0] >= date:
                 print(values)
+
+    def get_top_list(self):
+        steam_market = requests.get(
+            f'https://steamcommunity.com/market/search?appid=730#p5_popular_desc')
+        self.html_site = bs4.BeautifulSoup(steam_market.text, 'html.parser')
+        print(self.html_site.find_all('div', id='searchResultsRows'))
 
 
 steam = SteamMarket()
